7aa5d303d2948322322bed4a1a43f3b43711d15f
Jake Hackl <jakerhackl@gmail.com>
2025-02-05 09:16:25 -0600

    Initial commit

---
+++ KeyboardWatcherApplicationContext.cs
@@ -0,0 +1,42 @@
+﻿namespace ProArtKeyboardWatcher;
+
+public class KeyboardWatcherApplicationContext : ApplicationContext
+{
+    private readonly NotifyIcon _trayIcon;
+    private readonly HiddenForm _hiddenForm;
+
+    public KeyboardWatcherApplicationContext()
+    {
+        // Initialize the hidden window
+        _hiddenForm = new HiddenForm();
+
+        // Create a context menu for the tray icon
+        var contextMenu = new ContextMenuStrip();
+        contextMenu.Items.Add("Exit", null, Exit!);
+
+        // Initialize the tray icon
+        _trayIcon = new NotifyIcon()
+        {
+            Icon = SystemIcons.GetStockIcon(StockIconId.DriveRam, StockIconOptions.ShellIconSize),
+            ContextMenuStrip = contextMenu,
+            Visible = true,
+            Text = "ProArt Keyboard Watcher",
+        };
+
+        // Register for device notifications
+        _hiddenForm.RegisterForDeviceNotifications();
+    }
+
+    // Exit event handler for the tray icon
+    private void Exit(object sender, EventArgs e)
+    {
+        // Hide tray icon before exit
+        _trayIcon.Visible = false;
+
+        // Close the hidden form
+        _hiddenForm.Close();
+
+        // Exit the application
+        Application.Exit();
+    }
+}
